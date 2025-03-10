9cc7a2f64d38eabaff299b900e1c137d9cd29613
TusharKoshti-1 <tusharkoshti01@gmail.com>
2025-03-10 11:07:54 +0530

    addorder

--- src/components/sections/dashboard/order/addorder.tsx
+++ src/components/sections/dashboard/order/addorder.tsx
@@ -184,7 +184,7 @@ const AdminAddOrderPage: React.FC = () => {
                 <Grid item key={item.id} xs={12} sm={6} md={4} lg={3}>
                   <Card
                     sx={{
-                      maxWidth: 345,
+                      maxWidth: 700,
                       height: '100%',
                       display: 'flex',
                       flexDirection: 'column',
