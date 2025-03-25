74b216c83d5d8b6a6a61889b3bc298e26d3aa283
TusharKoshti-1 <tusharkoshti01@gmail.com>
2025-03-25 14:22:29 +0530

    fix

--- src/pages/table-order/TableOrder.tsx
+++ src/pages/table-order/TableOrder.tsx
@@ -3,7 +3,7 @@ import TableOrder from 'components/sections/dashboard/table-order/tableorder';
 
 const tableorder = () => {
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={2} sx={{ display: 'block' }}>
       <TableOrder />
     </Grid>
   );
